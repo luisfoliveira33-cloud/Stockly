346159b4b3c8c82d47277ae5677a34c452ccb93c
Luiszy77 <luis.f.oliveira33@aluno.senai.br>
2026-03-16 09:30:39 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,8 @@ authDomain: "stocky-53f07.firebaseapp.com",
 projectId: "stocky-53f07",
 storageBucket: "stocky-53f07.firebasestorage.app",
 messagingSenderId: "904523452324",
-appId: "1:904523452324:web:5b44b780b55f5f265a7b31"
+appId: "1:904523452324:web:5b44b780b55f5f265a7b31",
+measurementId: "G-PNF8EYR81G"
 };
 
 export const app = initializeApp(firebaseConfig);
